d8cd0e2aecb006af4508a65759ccb63e29ef0e81
Vladimir Gorbenko <volodg@mail.ru>
2012-06-21 13:01:29 +0300

    remove redundant third party libs

--- src/lib-third-party/ObjcScopedGuard/ObjcScopedGuard/ObjcScopedGuard/ObjcGuardBlocks.h
+++
@@ -1,6 +0,0 @@
-#ifndef ObjcScopedGuard_ObjcGuardBlocks_h
-#define ObjcScopedGuard_ObjcGuardBlocks_h
-
-typedef void (^GuardCallbackBlock)(void);
-
-#endif
--- src/lib-third-party/ObjcScopedGuard/ObjcScopedGuard/ObjcScopedGuard/ObjcReferendeCountGuard.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#include <ObjcScopedGuard/ObjcGuardBlocks.h>
-
-@interface ObjcReferendeCountGuard : NSObject
-
--(id)initWithBlock:( GuardCallbackBlock )block_;
--(void)releaseGuard;
-
-@end
