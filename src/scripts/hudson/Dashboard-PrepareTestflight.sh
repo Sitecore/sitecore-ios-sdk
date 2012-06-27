fa6c85f02833748fc86ac0fc32c2677cd0e68f3c
Vladimir Gorbenko <volodg@mail.ru>
2012-06-27 15:45:58 +0300

    remove redundant files

--- src/scripts/hudson/Dashboard-PrepareTestflight.sh
+++
@@ -1,8 +0,0 @@
-
-LAUNCH_DIR=$PWD
-BUILT_PRODUCTS_DIR=$LAUNCH_DIR/app/Dashboard/build/Release-iphoneos/
-
-
-cd "$LAUNCH_DIR/scripts"
-   /bin/bash "$PWD/UploadToTestFlight.sh" "$BUILT_PRODUCTS_DIR"
-cd "$LAUNCH_DIR"
