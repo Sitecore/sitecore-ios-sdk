3c2d44f6b35e93427dfa2d8936386f9bb180adb5
dodikk <dodikk88.reg@gmail.com>
2012-09-04 15:03:27 +0300

    Added missing "extern C" statements

--- src/lib/SCWebPlugins/SCWebPlugins/Details/pathToAllPluginsJavascripts.h
+++ src/lib/SCWebPlugins/SCWebPlugins/Details/pathToAllPluginsJavascripts.h
@@ -3,7 +3,14 @@
 
 @class NSString, NSArray;
 
-NSArray* scWebPluginsClasses( void );
-NSString* pathToAllPluginsJavascripts( void );
+#ifdef __cplusplus
+extern "C"
+{
+#endif //__cplusplus
+    NSArray* scWebPluginsClasses( void );
+    NSString* pathToAllPluginsJavascripts( void );
+#ifdef __cplusplus
+}
+#endif //__cplusplus
 
 #endif //SC_PATH_TO_ALL_PLUGINS_JAVASCRIPT_EXPORT_INCLUDED_H
