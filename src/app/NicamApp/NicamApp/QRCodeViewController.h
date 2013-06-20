37462c8b752fa35d8a89f5f77ac21a6a7b156834
dodikk <dodikk88.reg@gmail.com>
2013-06-20 10:01:43 +0300

    Removed Nicam.app

--- src/app/NicamApp/NicamApp/QRCodeViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@class SCQRCodeReaderView;
-
-@interface QRCodeViewController : UIViewController < SCQRCodeReaderViewDelegate >
-
-@property(nonatomic,strong) IBOutlet SCQRCodeReaderView *qrcodeView;
-
-@end
