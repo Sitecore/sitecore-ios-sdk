c2911d07566296e8707870369a6db05c49360886
dodikk <dodikk88.reg@gmail.com>
2013-10-17 13:28:29 +0300

    [async] Added new operations.

--- src/lib/SCUtils/SitecoreMobileSDK/SCUtils.h
+++ src/lib/SCUtils/SitecoreMobileSDK/SCUtils.h
@@ -1,3 +1,8 @@
 
 #import <SitecoreMobileSDK/NSArray+SCBlocksAdditions.h>
 #include <SitecoreMobileSDK/SCAsyncOpDefinitions.h>
+#include <SitecoreMobileSDK/SCExtendedOperations.h>
+
+#include <SitecoreMobileSDK/SCAsyncOpRelationsBuilder.h>
+#include <SitecoreMobileSDK/SCExtendedAsyncOpRelationsBuilder.h>
+
