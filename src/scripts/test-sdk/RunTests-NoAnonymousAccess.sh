bae1d218bf3c6e1736e4cd73fb59d1e558d5d3d9
dodikk <dodikk88.reg@gmail.com>
2013-10-17 11:15:29 +0300

    [CI] Split test cases for enabled and disabled anonymous access

--- src/scripts/test-sdk/RunTests-NoAnonymousAccess.sh
+++ src/scripts/test-sdk/RunTests-NoAnonymousAccess.sh
@@ -21,25 +21,17 @@ OLD_XCODE_PATH=$(xcode-select -print-path)
 #latest stable xcode
 #sudo xcode-select -switch "/Applications/Xcode/Contents/Developer"
 
+
 rm -rf "$PROJECT_ROOT/deployment"
 mkdir -p "$PROJECT_ROOT/deployment/test-results"
 
 /bin/bash "$SCRIPTS_ROOT_DIR/simulator/CleanTestReports.sh"
-    /bin/bash "$PWD/RunSCContactTest.sh" "$IOS_VERSION" "$CONFIGURATION"
+    /bin/bash "$PWD/RunSCApiTestCleanSitecore-NoAnonymous.sh" "$IOS_VERSION" "$CONFIGURATION"
     if [ "$?" -ne "0" ]; then 
-       echo "[!!! ERROR !!!] : RunSCContactTest.sh failed"
+       echo "[!!! ERROR !!!] : RunSCApiTestCleanSitecore.sh failed"
        exit 1
     fi
-
-
-    /bin/bash "$PWD/RunSCMapViewTests.sh" "$IOS_VERSION" "$CONFIGURATION"
-    if [ "$?" -ne "0" ]; then 
-       echo "[!!! ERROR !!!] : RunSCMapViewTests.sh failed"
-       exit 1
-    fi
-
 /bin/bash "$SCRIPTS_ROOT_DIR/simulator/CopyTestReports.sh"
 
 #restore settings
 #sudo xcode-select -switch "$OLD_XCODE_PATH"
-
