c3a030af12ecaa3d2c82efd4989f2827288af6fc
dodikk <dodikk88.reg@gmail.com>
2013-10-17 13:09:21 +0300

    [field record] Using c++

--- src/lib/SCApi/SCApi/Data/SCFieldRecord.mm
+++ src/lib/SCApi/SCApi/Data/SCFieldRecord.mm
@@ -11,15 +11,21 @@
 #import "SCDroplinkField.h"
 #import "SCDroptreeField.h"
 #import "SCGeneralLinkField.h"
+#import "SCRichTextField.h"
+
+#import "SCItemRecord+SCItemSource.h"
 
 @interface SCField (SCFieldRecord)
 
 +(id)fieldWithFieldRecord:( SCFieldRecord* )fieldRecord_
-               apiContext:( SCApiContext* )apiContext_;
+               apiContext:( SCExtendedApiContext* )apiContext_;
 
 @end
 
 @implementation SCFieldRecord
+
+@dynamic itemSource;
+@dynamic itemId;
 
 +(Class)fieldClassForType:( NSString* )type_
 {
@@ -27,6 +33,7 @@
     if ( !classByFieldType_ )
     {
         classByFieldType_ = @{
+        @"Rich Text"    : [ SCRichTextField    class ],
         @"Image"        : [ SCImageField       class ],
         @"Checkbox"     : [ SCCheckboxField    class ],
         @"Date"         : [ SCDateField        class ],
@@ -57,6 +64,45 @@
     }
 
     return field_;
+}
+
+-(BOOL)isEqual:(id)object
+{
+    JUncertainLogicStates pointerCheckResult = [ NSObject quickCheckObject: self
+                                                                 isEqualTo: object ];
+    if ( ULMaybe != pointerCheckResult )
+    {
+        return (BOOL)pointerCheckResult;
+    }
+    
+
+    SCFieldRecord* otherField = objc_kind_of_cast<SCFieldRecord>( object );
+    if ( nil == otherField )
+    {
+        return NO;
+    }
+    
+    
+    BOOL result =
+       [ NSObject object: self.fieldId  isEqualTo: otherField.fieldId  ]
+    && [ NSObject object: self.name     isEqualTo: otherField.name     ]
+    && [ NSObject object: self.type     isEqualTo: otherField.type     ]
+    && [ NSObject object: self.rawValue isEqualTo: otherField.rawValue ];
+    
+    return result;
+}
+
+
+#pragma mark -
+#pragma mark Dynamic
+-(SCItemSourcePOD*)itemSource
+{
+    return self.itemRecord.itemSource;
+}
+
+-(NSString*)itemId
+{
+    return self.itemRecord.itemId;
 }
 
 @end
