2db5c792f9412e1fbd849bbef89583db974548b5
dodikk <dodikk88.reg@gmail.com>
2013-10-17 12:38:57 +0300

    Added ifdef for encryption switch

---
+++ src/lib/SCApi/SCApi/Utils/SCApiDefines.h
@@ -0,0 +1,9 @@
+#ifndef SCApi_SCApiDefines_h
+#define SCApi_SCApiDefines_h
+
+    #ifdef IS_ENCRYPTION_ENABLED
+    #undef IS_ENCRYPTION_ENABLED
+    #endif
+    #define IS_ENCRYPTION_ENABLED 1
+
+#endif
