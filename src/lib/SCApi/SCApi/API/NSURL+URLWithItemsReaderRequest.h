10eae9b8bc414e17bc8294c357458d652e0c8720
dodikk <dodikk88.reg@gmail.com>
2013-10-17 11:54:45 +0300

    URL construction logic refactoring

--- src/lib/SCApi/SCApi/API/NSURL+URLWithItemsReaderRequest.h
+++ src/lib/SCApi/SCApi/API/NSURL+URLWithItemsReaderRequest.h
@@ -1,31 +1,6 @@
 #import <Foundation/Foundation.h>
 
-@class SCApiContext;
-@class SCCreateItemRequest;
-@class SCItemsReaderRequest;
-@class SCCreateMediaItemRequest;
-
 @interface NSURL (URLWithItemsReaderRequest)
-
-+(id)URLWithItemsReaderRequest:( SCItemsReaderRequest* )request_
-                          host:( NSString* )host_;
-
-+(id)URLToEditItemsWithRequest:( SCItemsReaderRequest* )request_
-                          host:( NSString* )host_;
-
-+(id)URLToCreateItemWithRequest:( SCCreateItemRequest* )createItemRequest_
-                           host:( NSString* )host_;
-
-+(id)URLToCreateMediaItemWithRequest:( SCCreateMediaItemRequest* )createItemRequest_
-                                host:( NSString* )host_
-                          apiContext:( SCApiContext* )apiContext_;
-
-+(id)URLToGetSecureKeyForHost:( NSString* )host_;
-
-+(id)URLToGetRenderingHTMLLoaderForRenderingId:( NSString* )rendereringId_
-                                      sourceId:( NSString* )sourceId_
-                                          host:( NSString* )host_
-                                    apiContext:( SCApiContext* )apiContext_;
 
 +(id)URLToTriggerAction:( NSString* )itemPath_
               paramName:( NSString* )paramName_
