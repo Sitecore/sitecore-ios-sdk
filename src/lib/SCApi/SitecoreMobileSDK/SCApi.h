c679b62933c7bd835db77d8cd03a51e32bb78e36
dodikk <dodikk88.reg@gmail.com>
2013-10-17 11:38:02 +0300

    Added new classes to a root header file

--- src/lib/SCApi/SitecoreMobileSDK/SCApi.h
+++ src/lib/SCApi/SitecoreMobileSDK/SCApi.h
@@ -6,13 +6,19 @@
 //  Copyright 2012. Sitecore. All rights reserved.
 //
 
+#import <SitecoreMobileSDK/SCItemSource.h>
+#import <SitecoreMobileSDK/SCItemSourcePOD.h>
+
 #import <SitecoreMobileSDK/SCApiContext.h>
+#import <SitecoreMobileSDK/SCExtendedApiContext.h>
 
 #import <SitecoreMobileSDK/SCItem.h>
 #import <SitecoreMobileSDK/SCPagedItems.h>
 
+#import <SitecoreMobileSDK/SCBaseItemRequest.h>
 #import <SitecoreMobileSDK/SCEditItemsRequest.h>
 #import <SitecoreMobileSDK/SCItemsReaderRequest.h>
+#import <SitecoreMobileSDK/SCHTMLReaderRequest.h>
 #import <SitecoreMobileSDK/SCCreateItemRequest.h>
 #import <SitecoreMobileSDK/SCCreateMediaItemRequest.h>
 #import <SitecoreMobileSDK/SCPageEventTriggeringRequest.h>
@@ -30,5 +36,8 @@
 #import <SitecoreMobileSDK/SCDroptreeField.h>
 #import <SitecoreMobileSDK/SCFieldLinkData.h>
 #import <SitecoreMobileSDK/SCGeneralLinkField.h>
+
+#import <SitecoreMobileSDK/SCParams.h>
+#import <SitecoreMobileSDK/SCFieldImageParams.h>
 
 #import <SitecoreMobileSDK/SCError.h>
