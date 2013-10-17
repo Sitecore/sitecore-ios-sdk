fdfc9037696b6e7c7b48de6b45e038b6a964c975
dodikk <dodikk88.reg@gmail.com>
2013-10-17 13:15:45 +0300

    Added ifdef for cache switch

---
+++ src/lib/SCApi/SCApi/SCApiMacro.h
@@ -0,0 +1,9 @@
+#ifndef SCApi_SCApiMacro_h
+#define SCApi_SCApiMacro_h
+
+#ifdef USE_IN_MEMORY_CACHE
+#undef USE_IN_MEMORY_CACHE
+#endif // USE_IN_MEMORY_CACHE
+#define USE_IN_MEMORY_CACHE 1
+
+#endif //SCApi_SCApiMacro_h
