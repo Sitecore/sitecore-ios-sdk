d4a22c05d3eeb2db7186e707d2d0a53dfd6d8855
dodikk <dodikk88.reg@gmail.com>
2013-10-17 13:04:45 +0300

    Added rich text field support

---
+++ src/lib/SCApi/SCApi/Data/SCFields/SCRichTextField.h
@@ -0,0 +1,4 @@
+#import "SCField.h"
+
+@interface SCRichTextField : SCField
+@end
---
+++ src/lib/SCApi/SCApi/Data/SCFields/SCRichTextField.m
@@ -0,0 +1,60 @@
+#import "SCRichTextField.h"
+
+#import "NSString+URLWithItemsReaderRequest.h"
+#import "SCExtendedApiContext.h"
+
+
+@implementation SCRichTextField
+
+-(id)createFieldValue
+{
+    NSString* rawValue = self.rawValue;
+
+    static NSString* const SITECORE_MEDIA = @"~/media";
+    NSString* host = [ self.apiContext.host scHostWithURLScheme ];
+    
+    NSRange entireString = { 0, rawValue.length };
+
+    #define WHITESPACE @"[ ]*?"
+    NSString* imgTagPatternBase =
+    @"(?:"
+        @"("
+            @"<img"  // opening tag
+            @"[^>]*?" // all but close tag symbol
+            @"src"   // "src" attribute
+            WHITESPACE  // whitespaces
+            @"="     // value separator
+            WHITESPACE  // whitespaces
+            @"[\"|']"    // single or double quote
+            WHITESPACE  // whitespaces
+        @")"
+    @")";
+    
+    NSString* imgTagPattern = [ NSString stringWithFormat:@"%@(%@)", imgTagPatternBase, SITECORE_MEDIA ];
+    
+    
+//    0 <img alt="" height="100" width="100" src=" ~/media
+//    1 <img alt="" height="100" width="100" src="
+//    2 ~/media
+    NSString* replacementPattern = [ NSString stringWithFormat: @"$1%@/$2", host ];
+    
+    NSError* regexConstructorError = nil;
+    NSRegularExpression* transform =
+    [[ NSRegularExpression  alloc ] initWithPattern: imgTagPattern
+                                            options: NSRegularExpressionCaseInsensitive
+                                              error: &regexConstructorError ];
+    NSParameterAssert( nil == regexConstructorError );
+    NSParameterAssert( nil != transform );
+
+    
+    
+    NSString* fixedUrls =
+    [ transform stringByReplacingMatchesInString: rawValue
+                                         options: 0
+                                           range: entireString
+                                    withTemplate: replacementPattern ];    
+    
+    return fixedUrls;
+}
+
+@end
