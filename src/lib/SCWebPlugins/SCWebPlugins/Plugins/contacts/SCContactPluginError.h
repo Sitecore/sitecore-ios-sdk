d695276c2573b01aa27106134b90bec9e0667b9f
dodikk <dodikk88.reg@gmail.com>
2013-10-28 17:22:39 +0200

    [contacts leak] Added error class

---
+++ src/lib/SCWebPlugins/SCWebPlugins/Plugins/contacts/SCContactPluginError.h
@@ -0,0 +1,13 @@
+//
+//  SCContactPluginError.h
+//  SCWebPlugins
+//
+//  Created by Oleksandr Dodatko on 10/28/13.
+//
+//
+
+#import "SCWebPluginError.h"
+
+@interface SCContactPluginError : SCWebPluginError
+
+@end
---
+++ src/lib/SCWebPlugins/SCWebPlugins/Plugins/contacts/SCContactPluginError.m
@@ -0,0 +1,13 @@
+//
+//  SCContactPluginError.m
+//  SCWebPlugins
+//
+//  Created by Oleksandr Dodatko on 10/28/13.
+//
+//
+
+#import "SCContactPluginError.h"
+
+@implementation SCContactPluginError
+
+@end
