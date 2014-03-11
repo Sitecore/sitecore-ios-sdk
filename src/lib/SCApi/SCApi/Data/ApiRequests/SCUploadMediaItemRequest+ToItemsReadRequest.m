5a3627fe86b563e03256c863789c9f78bd8d8d01
dodikk <dodikk88.reg@gmail.com>
2014-03-11 12:57:35 +0200

    [media] Using root from context.
    Using lowercase too

--- src/lib/SCApi/SCApi/Data/ApiRequests/SCUploadMediaItemRequest+ToItemsReadRequest.m
+++ src/lib/SCApi/SCApi/Data/ApiRequests/SCUploadMediaItemRequest+ToItemsReadRequest.m
@@ -1,19 +1,42 @@
 #import "SCUploadMediaItemRequest+ToItemsReadRequest.h"
 
-#import "SCApiSession.h"
+#import "SCExtendedApiSession.h"
 #import "SCReadItemsRequest+Factory.h"
 
+
+
 @implementation SCUploadMediaItemRequest (ToItemsReadRequest)
+
+-(NSLocale*)posixLocale
+{
+    static NSLocale* result = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^void()
+    {
+        result = [ [ NSLocale alloc ] initWithLocaleIdentifier: @"en_US_POSIX" ];
+    });
+    
+    return result;
+}
 
 -(SCReadItemsRequest *)toItemsReadRequestWithApiSession:( SCExtendedApiSession* )apiSession_
 {
     SCReadItemsRequest * result_ = [SCReadItemsRequest new];
     result_ = [ result_ itemsReaderRequestWithApiSession: apiSession_ ];
     result_.fieldNames = [ self.fieldNames copy ];
-    result_.request = [ @"/sitecore/Media Library" stringByAppendingPathComponent: self.folder ];
+    
+    NSString* mediaPath = apiSession_.mediaLibraryPath;
+    NSParameterAssert( nil != mediaPath );
+    
+    
+    
+    result_.request = [ mediaPath stringByAppendingPathComponent: self.folder ];
+    result_.request = [ result_.request lowercaseStringWithLocale: [ self posixLocale ] ];
+    
     result_.requestType = SCReadItemRequestItemPath;
 
     return result_;
 }
 
 @end
+
