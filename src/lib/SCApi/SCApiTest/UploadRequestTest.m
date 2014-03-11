e564eece4ebd0b5f14291606c4a6a24c3f7565b2
dodikk <dodikk88.reg@gmail.com>
2014-03-11 15:17:45 +0200

    [media] Added tests for SCCreateMediaRequestUrlBuilder

--- src/lib/SCApi/SCApiTest/UploadRequestTest.m
+++ src/lib/SCApi/SCApiTest/UploadRequestTest.m
@@ -1,6 +1,9 @@
 #import <XCTest/XCTest.h>
 
+#import "SCCreateMediaRequestUrlBuilder.h"
 #import "SCUploadMediaItemRequest+ToItemsReadRequest.h"
+
+static NSString* const MEDIA_ROOT_FOR_URL_BUILDER = @"/fotki/kompromat";
 
 @interface UploadRequestTest : XCTestCase
 @end
@@ -13,6 +16,8 @@
     SCExtendedApiSession* _apiSession   ;
     
     SCUploadMediaItemRequest* _request;
+    SCCreateMediaRequestUrlBuilder* _defaultUrlBuilder;
+    SCCreateMediaRequestUrlBuilder* _otherPathUrlBuilder;
 }
 
 
@@ -33,6 +38,23 @@
         request.folder = @"/cats/XYZ";
     }
     self->_request = request;
+    
+    
+    self->_defaultUrlBuilder =
+    [ [ SCCreateMediaRequestUrlBuilder alloc ] initWithHost: @"localhost"
+                                           mediaLibraryRoot: [ SCApiSession defaultMediaLibraryPath ]
+                                              webApiVersion: @"v1"
+                                              restApiConfig: [ SCWebApiConfig webApiV1Config ]
+                                                    request: self->_request ];
+
+
+    self->_otherPathUrlBuilder =
+    [ [ SCCreateMediaRequestUrlBuilder alloc ] initWithHost: @"localhost"
+                                           mediaLibraryRoot: MEDIA_ROOT_FOR_URL_BUILDER
+                                              webApiVersion: @"v1"
+                                              restApiConfig: [ SCWebApiConfig webApiV1Config ]
+                                                    request: self->_request ];
+
 }
 
 -(void)tearDown
@@ -49,6 +71,31 @@
  
     XCTAssertTrue( result.requestType == SCReadItemRequestItemPath, @"request type mismatch" );
     XCTAssertEqualObjects( result.request, @"/sitecore/media library/cats/xyz", @"request string mismatch" );
+}
+
+-(void)testChangedMediaPathIsPrependedToFolder
+{
+    self->_apiSession.mediaLibraryPath = @"/ololo/trololo/медия паз";
+    SCReadItemsRequest* result = [ self->_request toItemsReadRequestWithApiSession: self->_apiSession ];
+    
+    XCTAssertTrue( result.requestType == SCReadItemRequestItemPath, @"request type mismatch" );
+    XCTAssertEqualObjects( result.request, @"/ololo/trololo/медия паз/cats/xyz", @"request string mismatch" );
+}
+
+-(void)testCreateMediaUrlBuilderPrependsDefaultMediaRoot
+{
+    NSString* result = [ self->_defaultUrlBuilder getRequestUrl ];
+    NSString* expected = @"localhost/-/item/v1%2Fsitecore%2Fmedia%20library%2Fcats%2Fxyz?name=Grumpy%20Cat&template=%2Fsystem%2Fmedia%2Funversioned%2Fjpeg&payload=content";
+    
+    XCTAssertEqualObjects( result, expected, @"incorrect request" );
+}
+
+-(void)testCreateMediaUrlBuilderPrependsChangedMediaRoot
+{
+    NSString* result = [ self->_otherPathUrlBuilder getRequestUrl ];
+    NSString* expected = @"localhost/-/item/v1%2Ffotki%2Fkompromat%2Fcats%2Fxyz?name=Grumpy%20Cat&template=%2Fsystem%2Fmedia%2Funversioned%2Fjpeg&payload=content";
+    
+    XCTAssertEqualObjects( result, expected, @"incorrect request" );
 }
 
 @end
