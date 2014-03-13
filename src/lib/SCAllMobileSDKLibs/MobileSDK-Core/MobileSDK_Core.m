4e27904146bb0cfc1cf2209aa7e75a745bb29b13
dodikk <dodikk88.reg@gmail.com>
2014-03-13 15:08:09 +0200

    [split] Added targets for primary SDK parts

---
+++ src/lib/SCAllMobileSDKLibs/MobileSDK-Core/MobileSDK_Core.m
@@ -0,0 +1,13 @@
+//
+//  MobileSDK_Core.m
+//  MobileSDK-Core
+//
+//  Created by Oleksandr Dodatko on 3/13/14.
+//
+//
+
+#import "MobileSDK_Core.h"
+
+@implementation MobileSDK_Core
+
+@end
---
+++ src/lib/SCAllMobileSDKLibs/MobileSDK-JavaScript/MobileSDK_JavaScript.m
@@ -0,0 +1,13 @@
+//
+//  MobileSDK_JavaScript.m
+//  MobileSDK-JavaScript
+//
+//  Created by Oleksandr Dodatko on 3/13/14.
+//
+//
+
+#import "MobileSDK_JavaScript.h"
+
+@implementation MobileSDK_JavaScript
+
+@end
