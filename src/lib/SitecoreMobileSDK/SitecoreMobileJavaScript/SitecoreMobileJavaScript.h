70373273922ea320f914e2a4c50a8aa3bf362ab8
dodikk <dodikk88.reg@gmail.com>
2014-03-13 15:51:10 +0200

    [split] Created primary headers for new artefacts

---
+++ src/lib/SitecoreMobileSDK/SitecoreMobileJavaScript/SitecoreMobileJavaScript.h
@@ -0,0 +1,19 @@
+
+
+#import <SitecoreMobileJavaScript/SCWebView.h>
+#import <SitecoreMobileJavaScript/SCWebViewDelegate.h>
+
+#import <SitecoreMobileJavaScript/SCWebBrowser.h>
+#import <SitecoreMobileJavaScript/SCWebBrowserDelegate.h>
+
+#import <SitecoreMobileJavaScript/SCWebBrowserToolbar.h>
+
+
+
+#import <SitecoreMobileUI/SitecoreMobileUI.h>
+#import <SitecoreMobileSDK/SitecoreMobileSDK.h>
+
+
+
+
+
