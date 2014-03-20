11b581b10943acfb6d9816a75131fb5e24f49ee9
dodikk <dodikk88.reg@gmail.com>
2014-03-20 17:10:03 +0200

    [appledoc] Fixed directories

--- src/scripts/test-sdk/BuildMobileSdkDocumentation.sh
+++ src/scripts/test-sdk/BuildMobileSdkDocumentation.sh
@@ -29,13 +29,11 @@ cd "$DEPLOYMENT_DIR"
 		--company-id net.sitecore \
 		--output . \
 		"$SDK_LIBRARIES_ROOT/SCApi/SitecoreMobileSDK" \
-		"$SDK_LIBRARIES_ROOT/SCMap/SitecoreMobileSDK" \
-		"$SDK_LIBRARIES_ROOT/SCQRCodeReader/SitecoreMobileSDK" \
-		"$SDK_LIBRARIES_ROOT/SCUI/SitecoreMobileSDK" \
+		"$SDK_LIBRARIES_ROOT/SCMap/SitecoreMobileUI" \
+		"$SDK_LIBRARIES_ROOT/SCQRCodeReader/SitecoreMobileUI" \
+		"$SDK_LIBRARIES_ROOT/SCUI/SitecoreMobileUI" \
 		"$SDK_LIBRARIES_ROOT/SCUtils/SitecoreMobileSDK" \
-		"$SDK_LIBRARIES_ROOT/SCWebView/SitecoreMobileSDK"
-#		"$SDK_LIBRARIES_ROOT/SCWebContentApi" \
-		"$SDK_LIBRARIES_ROOT/SCWebPlugins" \
+		"$SDK_LIBRARIES_ROOT/SCWebView/SitecoreMobileJavaScript"
 
 
 
