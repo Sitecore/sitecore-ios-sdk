36929f60f9038470d2d4ff041327abafbe646421
dodikk <dodikk88.reg@gmail.com>
2014-03-21 10:24:13 +0200

    [appledoc] Fixed grammar errors. Expanded some unclear comments.

--- src/lib/SCWebView/SitecoreMobileJavaScript/SCWebBrowser.h
+++ src/lib/SCWebView/SitecoreMobileJavaScript/SCWebBrowser.h
@@ -6,14 +6,14 @@
 /**
  The SCWebBrowser object contains SCWebView view and simple navigation bar.
  
- The interface of SCWebBrowser is similar to UIWebView and has the same behaviour.
+ The interface of SCWebBrowser is similar to UIWebView and has the same behaviour. It should be used to open web pages that use sitecore javascript web plug-ins. Otherwise they will not work properly.
  */
 @interface SCWebBrowser : UIView
 
 /**
  SCWebBrowser has simple navigation view with back and forward buttons and activity indicator and if you need to provide own custom navigator view, please pass UIView object which conforms to SCWebBrowserToolbar protocol.
  
- @param navigator the custom navigator UIView object
+ @param navigator a custom control that lets the user go back and forward through web pages displayed in the SCWebBrowser. See SCWebBrowserToolbar and SCWebBrowserToolbarDelegate protocols for more details.
  */
 -(void)setCustomToollbarView:(UIView< SCWebBrowserToolbar >*)toolbar;
 
@@ -23,105 +23,153 @@
 @property(nonatomic,readonly) UIActivityIndicatorView* activityIndicator;
 
 /**
- This property is alias to -[UIWebView delegate] property.
+ This property is an alias to -[UIWebView delegate] property.
  */
 @property(nonatomic,weak) id<SCWebBrowserDelegate> delegate;
 
 /**
- This property is alias to -[UIWebView scrollView] property.
+ This property is an alias to -[UIWebView scrollView] property.
  */
 @property(nonatomic,readonly) UIScrollView *scrollView;
 
 /**
- This method just calls -[UIWebView loadURL:[[NSURL alloc]initWithString: urlString]] method.
+ This method constructs an URL from the given string and calls the [UIWebView loadURL:] method.
+ 
+```
+   NSURL* urlToLoad = [[NSURL alloc]initWithString: urlString];
+   [UIWebView loadURL:urlToLoad];
+```
+
  @param urlString string with URL to open by browser
  */
 - (void)loadURLWithString:(NSString *)urlString;
+
+
 /**
- This method is alias to -[UIWebView loadRequest:] method.
+ This method is an alias to -[UIWebView loadRequest:] method.
  */
 - (void)loadRequest:(NSURLRequest *)request;
+
 /**
- This method is alias to -[UIWebView loadHTMLString:baseURL:] method.
+ This method is an alias to -[UIWebView loadHTMLString:baseURL:] method.
  */
 - (void)loadHTMLString:(NSString *)string baseURL:(NSURL *)baseURL;
+
+
 /**
- This method is alias to -[UIWebView loadURL:] method.
+ This method is an alias to -[UIWebView loadURL:] method.
  */
 - (void)loadURL:(NSURL *)url;
+
+
 /**
- This method is alias to -[UIWebView loadData:MIMEType:textEncodingName:baseURL:] method.
+ This method is an alias to -[UIWebView loadData:MIMEType:textEncodingName:baseURL:] method.
  */
 - (void)loadData:(NSData *)data MIMEType:(NSString *)MIMEType textEncodingName:(NSString *)textEncodingName baseURL:(NSURL *)baseURL;
 
+
 /**
- This property is alias to -[UIWebView request] property.
+ This property is an alias to -[UIWebView request] property.
  */
 @property(nonatomic,readonly) NSURLRequest *request;
 
+
 /**
- This method is alias to -[UIWebView reload] method.
+ This method is an alias to -[UIWebView reload] method.
  */
 - (void)reload;
+
+
 
 /**
  Refresh views
  */
 -(void)refreshViews;
+
+
+
 /**
- This method is alias to -[UIWebView stopLoading] method.
+ This method is an alias to -[UIWebView stopLoading] method.
  */
 - (void)stopLoading;
 
+
+
 /**
- This method is alias to -[UIWebView goBack] method.
+ This method is an alias to -[UIWebView goBack] method.
  */
 - (void)goBack;
+
+
+
 /**
- This method is alias to -[UIWebView goForward] method.
+ This method is an alias to -[UIWebView goForward] method.
  */
 - (void)goForward;
 
 /**
- This property is alias to -[UIWebView canGoBack] property.
+ This property is an alias to -[UIWebView canGoBack] property.
  */
 @property(nonatomic,readonly,getter=canGoBack) BOOL canGoBack;
+
+
+
 /**
- This property is alias to -[UIWebView canGoForward] property.
+ This property is an alias to -[UIWebView canGoForward] property.
  */
 @property(nonatomic,readonly,getter=canGoForward) BOOL canGoForward;
+
+
 /**
- This property is alias to -[UIWebView loading] property.
+ This property is an alias to -[UIWebView loading] property.
  */
 @property(nonatomic,readonly,getter=isLoading) BOOL loading;
 
+
+
 /**
- This method is alias to -[UIWebView stringByEvaluatingJavaScriptFromString:] method.
+ This method is an alias to -[UIWebView stringByEvaluatingJavaScriptFromString:] method.
  */
 - (NSString *)stringByEvaluatingJavaScriptFromString:(NSString *)script;
 
+
+
 /**
- This property is alias to -[UIWebView scalesPageToFit] property.
+ This property is an alias to -[UIWebView scalesPageToFit] property.
  */
 @property(nonatomic) BOOL scalesPageToFit;
 
+
+
 /**
- This property is alias to -[UIWebView dataDetectorTypes] property.
+ This property is an alias to -[UIWebView dataDetectorTypes] property.
  */
 @property(nonatomic) UIDataDetectorTypes dataDetectorTypes;
 
-/**
- This property is alias to -[UIWebView allowsInlineMediaPlayback] property.
- */
-@property (nonatomic) BOOL allowsInlineMediaPlayback; // iPhone Safari defaults to NO. iPad Safari defaults to YES
-/**
- This property is alias to -[UIWebView mediaPlaybackRequiresUserAction] property.
- */
-@property (nonatomic) BOOL mediaPlaybackRequiresUserAction; // iPhone and iPad Safari both default to YES
+
 
 /**
- This property is alias to -[UIWebView mediaPlaybackAllowsAirPlay] property.
+ This property is an alias to -[UIWebView allowsInlineMediaPlayback] property.
+ 
+ @return iPhone Safari defaults to NO. iPad Safari defaults to YES
  */
-@property (nonatomic) BOOL mediaPlaybackAllowsAirPlay; // iPhone and iPad Safari both default to YES
+@property (nonatomic) BOOL allowsInlineMediaPlayback;
+
+
+/**
+ This property is an alias to -[UIWebView mediaPlaybackRequiresUserAction] property.
+ 
+ @return iPhone and iPad Safari both default to YES
+ */
+@property (nonatomic) BOOL mediaPlaybackRequiresUserAction;
+
+
+/**
+ This property is an alias to -[UIWebView mediaPlaybackAllowsAirPlay] property.
+ 
+ @return iPhone and iPad Safari both default to YES
+ */
+@property (nonatomic) BOOL mediaPlaybackAllowsAirPlay;
+
 
 @end
