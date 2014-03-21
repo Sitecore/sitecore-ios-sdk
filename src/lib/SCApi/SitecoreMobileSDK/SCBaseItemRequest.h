5967e598656748cc89d9da9c3c5bd25cf29987ff
dodikk <dodikk88.reg@gmail.com>
2014-03-21 10:24:26 +0200

    [appledoc] Added class description

--- src/lib/SCApi/SitecoreMobileSDK/SCBaseItemRequest.h
+++ src/lib/SCApi/SitecoreMobileSDK/SCBaseItemRequest.h
@@ -8,12 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+
+/**
+ The SCBaseItemRequest class is a base for all web api request objects
+ 
+ * SCReadItemsRequest
+ * SCEditItemsRequest
+ * SCCreateItemRequest
+ * SCUploadMediaItemRequest
+ * SCGetRenderingHtmlRequest
+ 
+ It stores the information about the source of item or items being operated on. It conforms the SCItemSource protocol informally.
+ 
+ See the reference of SCItemSourcePOD class and SCItemSource protocol for more details.
+ */
 @interface SCBaseItemRequest : NSObject
 
 /**
- The source params used to request Sitecore items, if not set -[SCApiSession defaultLanguage] will be used.
+ The source params used to request Sitecore items, if not set the session defaults will be used.
  */
 @property( nonatomic, strong) SCItemSourcePOD* itemSource;
+
+
 
 /**
  The language used to request Sitecore items, if not set -[SCApiSession defaultLanguage] will be used.
@@ -32,9 +48,13 @@
  */
 @property( nonatomic, strong) NSString* site;
 
+
+
 /**
  The version used to request Sitecore items, if not set -[SCApiSession defaultItemVersion] will be used.
  */
 @property( nonatomic, strong) NSString* itemVersion;
+
+
 
 @end
