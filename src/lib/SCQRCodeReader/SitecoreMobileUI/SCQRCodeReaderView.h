c7614deb993c436b440bd627d73ebd0a3ab2d093
dodikk <dodikk88.reg@gmail.com>
2014-03-24 10:24:29 +0200

    [appledoc] Added missing notes on QR code scanning

--- src/lib/SCQRCodeReader/SitecoreMobileUI/SCQRCodeReaderView.h
+++ src/lib/SCQRCodeReader/SitecoreMobileUI/SCQRCodeReaderView.h
@@ -2,35 +2,51 @@
 
 @protocol SCQRCodeReaderViewDelegate;
 
+/**
+ A view to scan the QR code. It shows the camera capture data and a frame the QR code should fit in.
+ */
 @interface SCQRCodeReaderView : UIView
 
+
+
 /**
- captureRect specifies rect for direct scan result from.
- if captureRect was not specified SCQRCodeReaderView.bound will be used
+ Specifies a rect for direct scan result from.
+ If captureRect was not specified -[SCQRCodeReaderView bounds] will be used. This means that the marker will be searched for within the entire area of the captured frame.
 */
 @property (nonatomic) CGRect captureRect;
 
+
+
 /**
- delegate specifies object whith will get scan status chamges from SCQRCodeReaderView
+ Specifies an object that will get scan status changes from SCQRCodeReaderView
+ See SCQRCodeReaderViewDelegate protocol reference for more details.
 */
 @property (nonatomic,weak) IBOutlet id< SCQRCodeReaderViewDelegate > delegate;
+
+
+
 /**
  Rreates a new instance of SCQRCodeReaderView
- @param delegate_ the object which will handle events wfom SCQRCodeReaderView
- @param captureRect_ specifies rect for direct scan result from. 
+ 
+ @param delegate the object which will handle events wfom SCQRCodeReaderView
+ @param captureRect specifies rect for direct scan result from.
 */
-
-+ (id)viewWithDelegate:(id <SCQRCodeReaderViewDelegate>)delegate
-           captureRect:(CGRect)captureRect;
++ (instancetype)viewWithDelegate:(id <SCQRCodeReaderViewDelegate>)delegate
+                     captureRect:(CGRect)captureRect;
 
 /**
  Starts capture process
 */
 - (void)startCapture;
+
+
+
 /**
  Stope capture process
 */
 - (void)stopCapture;
+
+
 /**
  Call this method to determine capture is already running or not.
 */
--- src/lib/SCQRCodeReader/SitecoreMobileUI/SCQRCodeReaderViewDelegate.h
+++ src/lib/SCQRCodeReader/SitecoreMobileUI/SCQRCodeReaderViewDelegate.h
@@ -2,11 +2,19 @@
 
 @class SCQRCodeReaderView;
 
+
+
+/**
+ This protocol describes the events that will occur while scannign QR code data using the SCQRCodeReaderView widget.
+ */
 @protocol SCQRCodeReaderViewDelegate <NSObject>
 
 @optional
 /**
  This method calls when result is fully scaned
+ 
+ @param readerView A view that sent the event. If you have multiple instances of the SCQRCodeReaderView on your screen it will help you understand which instance has scanned the code.
+ @param resultString The data encoded in the QR code. The user is responsible for processing it properly.
  */
 - (void)qrCodeReaderView:(SCQRCodeReaderView *)readerView
         didGetScanResult:(NSString *)resultString;
