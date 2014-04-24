4e6e8f4b72025b25091de3887fb45f655cc3e742
dodikk <dodikk88.reg@gmail.com>
2014-04-24 16:01:06 +0300

    [CI] Updated test instances

--- src/test/FunctionalTests/SCApiTestCleanSitecore/SCApiTest/SCGlobalSettings.m
+++ src/test/FunctionalTests/SCApiTestCleanSitecore/SCApiTest/SCGlobalSettings.m
@@ -3,12 +3,12 @@
 const BOOL IS_ANONYMOUS_ACCESS_ENABLED = YES;
 
 //NSString* const SCWebApiHostName = @"http://ws-alr1.dk.sitecore.net:88";
-NSString* const SCWebApiHostName = @"http://ws-alr1.dk.sitecore.net:720";
+NSString* const SCWebApiHostName = @"http://mobiledev1ua1.dk.sitecore.net:704";
 
 
-NSString* const SCWebApiAnalyticsHostName = @"http://ws-alr1.dk.sitecore.net:720";
+NSString* const SCWebApiAnalyticsHostName = @"http://mobiledev1ua1.dk.sitecore.net:704";
 
-NSString* const SCWebApiHostWithAuthRequest = @"http://ws-alr1.dk.sitecore.net:720";
+NSString* const SCWebApiHostWithAuthRequest = @"http://mobiledev1ua1.dk.sitecore.net:704";
 
 NSString* const SCSitecoreShellSite = @"/sitecore/shell";
 
